c2a45782d3272f943737c8f45dc3f891b05fdfdc
kobori <46224993+TomoroKobori@users.noreply.github.com>
2021-06-16 09:01:46 +0900

    feat: headを移動

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,16 +1,9 @@
 import styles from './Layout.module.css'
-import Image from 'next/image'
-import Head from 'next/head'
 import Footer from './Footer'
 
 const Layout = props => {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>kb's Space</title>
-        <meta name="description" content="ここはkbのスペースです" />
-        <link rel="icon" href="/sam.png" />
-      </Head>
       {props.children}
       <Footer />
     </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 import { AppProps } from 'next/app'
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>kb's Space</title>
+        <meta name="description" content="ここはkbのスペースです" />
+        <link rel="icon" href="/sam.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default App
